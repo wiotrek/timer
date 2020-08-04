ad41d612aad5c5994328d1f243a13579d0822b6f
wiotrek <piotrwaszak97@gmail.com>
2020-08-04 11:39:16 +0200

    extand stopwatch class

--- src/app/timer/stopwatch.ts
+++ src/app/timer/stopwatch.ts
@@ -9,10 +9,42 @@ export class Stopwatch {
         return elapsedTime;
     }
 
-    display(): number {
+    display(): any {
         let elapsedTime = performance.now() - this.startTime;
         elapsedTime = Math.floor(elapsedTime / 10);
+        this.format(elapsedTime);
         return elapsedTime;
+    }
+
+    format(time: number): void {
+        let isEven: any = time % 2 ;
+        isEven = isEven ? true : false;
+
+        let amountPair = 0;
+
+        const timeArr: any = time.toString().split('');
+        console.log(timeArr.length);
+        console.log(isEven);
+
+        if (isEven) {
+            // how much is pair
+            amountPair = timeArr / 2;
+        }
+        let semiArr: [];
+        for (let i = 0; i < amountPair; i++) {
+            semiArr = [];
+            let quarterArr: any;
+            for (let j = amountPair; j < amountPair + 1; j++) {
+                quarterArr = array[j];
+            }
+        }
+
+        let i = 0;
+        while (i < amountPair + 2) {
+            i = + 2;
+            console.log('dupa');
+
+        }
     }
 
 }
